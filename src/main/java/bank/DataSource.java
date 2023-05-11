a6b4854f3b5d2aaa011326e86ed9b2be587d686b
REACTClass <118196893+REACTClass@users.noreply.github.com>
2023-05-11 04:16:47 +0000

    Create account query

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -30,6 +30,7 @@ public class DataSource {
 
         statement.setString(1, username);
         try(ResultSet resultSet = statement.executeQuery()) {
+          // Instantiate the customer object
           customer = new Customer(
             resultSet.getInt("id"), 
             resultSet.getString("name"),
@@ -43,8 +44,31 @@ public class DataSource {
     }
     return customer;
   }
+
+  public static Account getAccount(int accountId) {
+    String sql = "SELECT * FROM accounts WHERE id = ?";
+    Account account = null;
+
+    try(Connection connection = connect(); 
+      PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, accountId);
+      try(ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+          resultSet.getInt("id"),
+          resultSet.getString("type"),
+          resultSet.getDouble("balance"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return account;
+  }
+
   public static void main(String[] args) {
   Customer customer = getCustomer("twest8o@friendfeed.com");
   System.out.println(customer.getName());
+
+  Account account = getAccount(customer.getAccountId());
+  System.out.println(account.getBalance());
   }
 }
